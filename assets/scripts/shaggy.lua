a6beea1b56acae517028e48a76055d3be449da99
MatthewFro191919 <matthewfroncek4@gmail.com>
2024-12-06 16:20:14 -0500

    Add files via upload

---
+++ assets/scripts/shaggy.lua
@@ -0,0 +1,8 @@
+function onCreatePost()
+    if boyfriendName == 'shaggy' or boyfriendName == 'redshaggy' or boyfriendName  == 'supershaggy' or boyfriendName == 'godshaggy' or boyfriendName == 'dave-fnaf' then
+        setProperty('gfGroup.visible', false)
+   end
+    if boyfriendName == 'bf' then
+        setProperty('gfGroup.visible', true)
+    end
+end
